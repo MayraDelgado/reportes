bd152f68fa17201b7b73a36e33692b0d1e8ab208
MayraDelgado <31661546+MayraDelgado@users.noreply.github.com>
2018-04-04 12:58:25 -0500

    Update datosController.js

--- app/scripts/datosController.js
+++ app/scripts/datosController.js
@@ -59,39 +59,6 @@ var angularObj = {
                     console.log(error.message);
                 }
             }
-           /* $scope.consultaReporteFechas = function () {
-                //console.log("muestra mensaje timer");
-                try {
-                    swal({
-                        imageUrl: 'https://rawgit.com/MayraDelgado/reportes/master/app/img/cargando.gif',
-                        timer: 5000,
-                        showConfirmButton: false
-                    }).then(function (result) {
-                        console.log({
-                            start: moment($scope.Data.start).format('MM-DD-YYYY'),
-                            end: moment($scope.Data.end).format('MM-DD-YYYY')
-                        });
-                        //return;
-                        var conAjax = $http.post("https://cppa.metricamovil.com/PMFReports/DateReport", {
-                            start: moment($scope.Data.start).format('MM-DD-YYYY'),
-                            end: moment($scope.Data.end).format('MM-DD-YYYY')
-                        }, {
-                            headers: {
-                                'Content-Type': 'application/json'
-                            }
-                        }).then(function successCallback(response) {
-                            console.log(response);
-                            $scope.resultReporteFechas = response.data;
-                        }, function errorCallback(response) {
-                            console.error(response);
-                        });
-                    });
-
-
-                } catch (error) {
-                    console.log(error.message);
-                }
-            }*/
             
             $scope.fechasreport = function () {
                 swal({
@@ -118,50 +85,14 @@ var angularObj = {
 
                 });
             }
-
-            /*$scope.consultaVehiculos = function () {
-                try {
-                    //$scope.dispositivoSeleccionado = $scope.lstDeviceGeotab;
-                    var dispositivoSeleccionadoAux = this.dispositivoSeleccionado;
-                    if (dispositivoSeleccionadoAux.length > 0) {
-                        swal({
-                            imageUrl: "https://rawgit.com/MayraDelgado/reportes/master/app/img/cargando5.gif",
-                            timer: 5000,
-                            showConfirmButton: false
-                        }).then(function (result) {
-                            var listaIds = [];
-                            dispositivoSeleccionadoAux.forEach(function (dispositivo) {
-                                listaIds.push(dispositivo.id);
-                            });
-                            var conAjax = $http.post("https://cppa.metricamovil.com/PMFReports/DeviceReport", JSON.stringify({
-                                start: moment($scope.Data.start).format('MM-DD-YYYY'),
-                                end: moment($scope.Data.end).format('MM-DD-YYYY'),
-                                devices: listaIds
-                            }), {
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                }
-                            }).then(function successCallback(response) {
-                                $scope.resultConsultaVehiculos = response.data;
-                                console.log(response);
-                                if ($scope.resultConsultaVehiculos.length === 0) {
-                        swal({
-                            type: 'error',
-                            text: 'No existen registros en el rango de fechas seleccionado',
-                        });
-                    }
-                            }, function errorCallback(respone) {
-                                console.error(response);
-                            });
-                        });
-                    }
-
-                } catch (error) {
-                    console.log(error.message);
-                }
-            }*/
              $scope.vehiculosreport = function () {
                 var dispositivoSeleccionadoAux = this.dispositivoSeleccionado;
+                  if (dispositivoSeleccionadoAux.length === 0) {
+                    swal({
+                        type: 'error',
+                        text: 'Debes seleccionar al menos un vehículo para continuar la consulta !!',
+                    })
+                } else
                 if (dispositivoSeleccionadoAux.length > 0) {
                     swal({
                         imageUrl: 'https://rawgit.com/MayraDelgado/reportes/master/app/img/cargando5.gif',
@@ -212,31 +143,21 @@ var angularObj = {
                 }
             }
              
-           /* $scope.crearCSVFechas = function () {
-                $('#fechaInstalacion').table2excel({
-                    exclude: ".noExl",
-                    fileext: ".xls",
-                    filename: "AuditoríadeRegistros_Fechas" + new Date().toUTCString().replace(/[\-\:\.]/g, "")
-                });
-                limpiarTableFechas();
-            }*/
-           /* $scope.crearCSVvehiculo = function () {
-                $("#fechaDevice").table2excel({
-                    exclude: ".noExl",
-                    fileext: ".xls",
-                    filename: "AuditoríadeRegistros_Dispositivos"
-                });
-                limpiarTableDevices();
-            }*/
-            var limpiarTableFechas = function () {
-                // $('#fechaInstalacion tbody').remove();
-                location.reload();
-
+            $scope.crearCSVvehiculo = function () {
+                if ($scope.resultConsultaVehiculos.length === 0) {
+                    swal(
+                        '',
+                        'No hay datos que descargar',
+                        "error",
+                    )
+                    console.log("No hay datos que descargar");
+                } else
+                if ($scope.resultConsultaVehiculos.length > 0) {
+                    $("#fechaDevice").table2excel({
+                        filename: "AuditoríadeRegistros_Dispositivos"
+                    });
+                }
             }
-           /* var limpiarTableDevices = function () {
-                // $('#fechaDevice tbody').remove();
-                location.reload();
-            }*/
 
         }]);
 
