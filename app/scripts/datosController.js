ffe7a02cf5d4352a34e0fb00fbecc761d04ca021
MayraDelgado <31661546+MayraDelgado@users.noreply.github.com>
2018-05-04 15:44:05 -0500

    Update datosController.js

--- app/scripts/datosController.js
+++ app/scripts/datosController.js
@@ -166,18 +166,25 @@ var angularObj = {
                 return moment(date).format('MM-DD-YYYY');
             }
         });
-angularObj.app.directive('myOnFocus', function () {
+ angularObj.app.directive('myOnFocus', function () {
             return {
                 scope: true,
                 restrict: 'A',
                 link: function (scope, elem, attr, ctrl) {
+                    scope.showOptions = true;
+                    if ((attr['mdOnClose'])) {
+                        attr['mdOnClose'] = "showOptions=false;" + (attr['mdOnClose']);
+                    } else {
+                        (attr['mdOnClose']) = "showOptions=false;"
+                    }
                     elem.bind('focus', function () {
-                        if (scope[attr.myOnFocus]) {
+                        if (scope.showOptions) {
+                            console.log(scope, elem, attr, ctrl);
                             elem.triggerHandler('click');
                         }
                     });
                     elem.bind('blur', function () {
-                        scope[attr.myOnFocus] = true;
+                        scope.showOptions = true;
                     });
                 }
             };
