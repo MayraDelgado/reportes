4af4b983a115927e6173b9a1dd955dddb32e7801
MayraDelgado <31661546+MayraDelgado@users.noreply.github.com>
2018-05-04 15:50:53 -0500

    Update datosController.js

--- app/scripts/datosController.js
+++ app/scripts/datosController.js
@@ -2,14 +2,8 @@
 var angularObj = {
     app: null,
     initAngular: function (api, freshState) {
-        angularObj.app = angular.module('myAplicacion', ['ngMaterial', 'material.components.expansionPanels', 'md.data.table']).config([
-            '$provide',
-            function ($provide) {
-
-            }
-        ]);
-
-        angularObj.app.controller('accesoDatosController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+        angularObj.app = angular.module('myAplicacion', ['ngMaterial', 'material.components.expansionPanels', 'md.data.table']);
+        angularObj.app.controller('accesoDatosController', ['$scope', '$filter', '$http','$mdSelect', function ($scope, $filter, $http,$mdSelect) {
             $scope.lstDeviceGeotab = [];
             $scope.dispositivoSeleccionado = [];
             $scope.lstDevice = {};            
